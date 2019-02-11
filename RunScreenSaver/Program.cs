267188a5050949dfddb37d0212dd552e1d3e9ea8
jdj <jdj@elevisor.com>
2019-02-11 09:07:02 +0900

    init

---
+++ RunScreenSaver/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunScreenSaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("cmd.exe", "/c c:\\windows\\system32\\scrnsave.scr /s");
+            //Process.Start("C:\\Windows\\System32\\scrnsave.scr /s");
+
+            
+        }
+    }
+}
